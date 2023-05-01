e5ef104949863914cd30fb82c1ab37221ab13c58
Marcos-Auguusto <marcos.argos119@gmail.com>
2023-05-01 18:36:02 -0300

    create model

--- librarySearch/models.py
+++ librarySearch/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 
+class Book(models.Model):
+    book_title = models.CharField(max_length=250)
+    author = models.CharField(max_length=100)
+    book_cover = models.ImageField()
+    about = models.TextField()
+    isbn = models.IntegerField()
+    number_copies = models.DecimalField()
+    rented = models.BooleanField(default=True)
+    
