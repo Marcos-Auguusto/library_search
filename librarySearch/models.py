ec0db1f21f4914208425304527a28e2b9f7957b2
Marcos-Auguusto <marcos.argos119@gmail.com>
2023-05-01 21:23:51 -0300

    create model

--- librarySearch/models.py
+++ librarySearch/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
-class Book(models.Model):
-    book_title = models.CharField(max_length=250)
-    author = models.CharField(max_length=100)
-    book_cover = models.ImageField()
-    about = models.TextField()
-    isbn = models.IntegerField()
-    number_copies = models.DecimalField()
-    rented = models.BooleanField(default=True)
+class Livro(models.Model):
+    STATUS_CHOICES = (
+        ('disponivel','Disponivel'),
+        ('alugado','Alugado'),
+        ('reservado','Reservado'),
+    )
     
+    nome_livro = models.CharField(max_length=250)
+    autor = models.CharField(max_length=100)
+    capa_livro = models.ImageField()
+    sobre = models.TextField()
+    isbn = models.CharField(max_length=50)
+    numero_copias = models.CharField(max_length=10)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='disponivel')
+    
+    def __str__(self):
+        return self.nome_livro
